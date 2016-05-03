27f7609c084ffbb9f9cfa152f20aba73eb3d4e7a
kosty2979 <kosty2979@gmail.com>
2016-05-03 08:48:07 +0300

    works

---
+++ my works/js/lesson.js
@@ -0,0 +1,223 @@
+ var lessonHtml= [
+
+ {	theme: "HTML",
+	lessonNum: "1",
+	text: "Сверстать макет ",
+	liText:[
+		 "Ваша верстка должна выглядеть как можно более похожей к макету, т.е. должны совпадать размеры, расположение, цвета.",
+		 "Информацию (текст, фото) заменяйте на свою, так мы сможем быстрее познакомиться.",
+		 "Пока что верстку никуда не заливаем, пусть лежит у вас на компьютере. Это будем делать в слудеющем уроке.",
+	],
+	image:[
+			{
+				text: "Макет",
+				url: "image/homework1.png"
+			}
+
+		],
+link:{
+		urlImg: "image/1h.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work/1.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work/1.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "2",
+	text: " Залить на GIT",
+	liText:[
+		 "Разобраться с Github. Пройти <a href='http://githowto.com/ru'>туториал</a>, здесь так же можно найти инструкции по установке гита на вашу операционную систему.",
+		 "Залить первое домашнее задание в свой github-репозиторий."
+		
+	],
+	image:[
+			{
+				text: "Макет",
+				url: "image/homework1.png"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/1h.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work/1.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work/1.html"
+	}
+
+
+
+	},
+{	theme: "HTML",
+	lessonNum: "3",
+	text: " Сверстать макет Fitness",
+	liText:[
+		"Верстка должна выглядеть практически как на макете, но пиксельную точность пока не делаем.",
+		"Так же в этом задании есть изюминка. Возможно, вам нужно будет применить свойство float для создания двух колонок (сайдбар и контент).",
+		"Верхнее меню можно сделать через display: inline-block;, в ссылках есть описание данного свойства."
+		
+	],
+	image:[
+			{
+				text: "макет Fitness",
+				url: "image/homework3.jpg"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/3h.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%203v0/index.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%203v0/index.html"
+	}
+},
+
+{	theme: "HTML",
+	lessonNum: "4",
+	text: "Сверстать макет Asia Holidays Maldives",
+	liText:[
+		"Верстка должна выглядеть практически как на макете, но пиксельную точность пока не делаем.",
+		"В макете есть выпадающее меню, его нужно сделать с помощью position: absolute.",
+		"Так же есть три колонки, они должны быть одинаковой высоты вне зависимости от контента. Решение описано в <a href='http://xiper.net/collect/html-and-css-tricks/karkas-verstki/kolonki-ravnoy-visoty'>статье</a>. Можете взять простой способ решения данной задачи, и использовать фоновую картинку."
+		
+	],
+	image:[
+			{
+				text: "макет Asia ",
+				url: "image/homework4.jpg"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/h4.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%204/index.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%204/index.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "5_1",
+	text: "Сверстать макет Cadastre",
+	liText:[
+		"Верстка должна выглядеть практически как на макете, но пиксельную точность пока не делаем.",
+		"Темный полупрозрачный фон формы сделать через rgba().",
+		"Каждое поле формы должно подсвечиваться при фокусе, как показано на First Name.",
+		"В макете везде используется шрифт Tahoma, вместо него подключите Helvetica Neue",
+		"Форма должна быть отцентрирована по горизонтали и вертикали"
+		
+	],
+	image:[
+			{
+				text: "макет Cadastre",
+				url: "image/homework5_1.jpg"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/h5_1.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%205_2/home%20work%205_2.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%205_2/home%20work%205_2.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "5_2",
+	text: "Сверстать картину Пита Мондриана 'Композиция с красным, синим и желтым'",
+	liText:[
+		"Для этого нужно использовать таблицы.",
+		"Визуально верстка должна быть похожа на картину, но сохранение пропорций прямоугольников не обязательно, нужно использовать colspan и rowspan."
+		
+	],
+	image:[
+			{
+				text: "Картина",
+				url: "image/homework5_2.jpg"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/h5_2.jpg",
+		urlCode: "https://github.com/kosty2979/kosty2979.github.io/blob/master/markup/home%20work%205_1/home%20work%205_1.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%205_1/home%20work%205_1.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "6",
+	text: "Сверстать макет Zazor",
+	liText:[
+		"Верстка должна быть с пиксельной точностью.",
+		"Для разметки нужно использовать новые семантичные теги: header, footer, aside, nav, section, main, figure, figcaption.",
+		"Так же не забывайте о новых атрибутах форм, в хедере нужно применить placeholder=''.",
+		"Для создания всяческих теней, скругленных уголков и градиентов используем CSS3",
+		"Три формы после баннера можно не верстать"
+		
+	],
+	image:[
+			{
+				text: "макет Zazor",
+				url: "image/homework6.jpg"
+			}
+
+		],
+
+	link:{
+		urlImg: "image/h6.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%206/index.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%206/index.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "7_8",
+	text: "Сверстать макет Pingbuller",
+	liText:[
+		"Верстка должна быть резиновой, в архиве есть несколько макетов, демонстрирующих как должен выглядеть сайт при разной ширине экрана. Нужно использовать подход <a href='http://thumbnails.visually.netdna-cdn.com/AdaptiveWebDesignvsResponsiveWebDesign_52680bdb6c089_w1500.png'>Responsive</a> дизайна.",
+		"Верстка должна быть с пиксельной точностью.",
+		"Нужно использовать html5-теги.",
+		"html-сруктуру, названия классов и стилизацию делаем с помощью методологии БЭМ.",
+		"Слайдер только верстаем, никаких плагинов прикручивать не надо. Стрелки слайдера можете не делать"
+		
+	],
+	image:[
+			{
+				text: "для экрана 320px",
+				url: "image/Home_320.jpg"
+			},
+			{
+				text: "для экрана 480px",
+				url: "image/Home_480px.jpg"
+			},
+			{
+				text: "для экрана 768px",
+				url: "image/Home_768px.jpg"
+			},
+			{
+				text: "для экрана 960px",
+				url: "image/Home_960px.jpg"
+			}
+
+
+		],
+
+	link:{
+		urlImg: "image/h7.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%207-8/index.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%207-8/index.html"
+	}
+},
+{	theme: "HTML",
+	lessonNum: "9",
+	text: "",
+	liText:[
+		"Сделать макет с 7-8 домашки кроссбраузерным. Пиксельную точность на все браузеры делать не нужно, только для последнего хрома.",
+		"Поддержка нужна для IE8+, Firefox 5+, Opera 15+, Chrome latest."
+		
+	],
+
+
+	link:{
+		urlImg: "image/h7.jpg",
+		urlCode: "https://github.com/kosty2979/goit/blob/master/markup/home%20work%209/index.html",
+		urlPrew: "http://kosty2979.github.io/markup/home%20work%209/index.html"
+	}
+}
+
+]
